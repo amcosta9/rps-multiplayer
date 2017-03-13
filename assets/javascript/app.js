ed99273bb3bf73b8bea366febdf291c4f76e4b50
Ariel <ariel.m.little@gmail.com>
2017-03-12 22:52:21 -0400

    initial site files

---
+++ assets/javascript/app.js
@@ -0,0 +1,16 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyAgiYeKSZG8rx71aGn-B3jzWI4kcU68gLU",
+    authDomain: "rps-multi-4a65e.firebaseapp.com",
+    databaseURL: "https://rps-multi-4a65e.firebaseio.com",
+    storageBucket: "rps-multi-4a65e.appspot.com",
+    messagingSenderId: "447848230588"
+};
+firebase.initializeApp(config);
+
+// Var to reference database
+
+var database = firebase.database();
+
+
+
